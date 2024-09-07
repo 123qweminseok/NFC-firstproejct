f38998c00ffcbd840ad89bf9b12629be0da79ff4
Minseok Kim <everybetter@naver.com>
2024-09-07 21:50:29 +0900

    지금 날려요

--- app/src/main/java/com/minseok/tel/MainActivity.kt
+++ app/src/main/java/com/minseok/tel/MainActivity.kt
@@ -16,9 +16,10 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-//ddddd
+
 class MainActivity : AppCompatActivity() {
 
+    //gdgdgdgddddddddd 0947
     lateinit  var ManagerButton:Button //관리자 버튼
 
     private val REQUEST_CODE_READ_PHONE_STATE = 100
