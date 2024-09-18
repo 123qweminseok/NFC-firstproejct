f82aa40875f201557b602106d05dd89acb68b78d
[Github <[Github>
2024-09-19 00:02:36 +0900

    유심과 일치하는 번호라도 회원정보에 존재하지않으면 더이상 로그인을 할수없게 수정

--- app/src/main/java/com/minseok/tel/MainActivity.kt
+++ app/src/main/java/com/minseok/tel/MainActivity.kt
@@ -1,25 +1,35 @@
 package com.minseok.tel
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.telephony.SubscriptionManager
 import android.telephony.TelephonyManager
-import android.widget.Button
-import android.widget.EditText
+import android.util.Base64
+import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.minseok.tel.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import javax.crypto.BadPaddingException
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
 class MainActivity : AppCompatActivity() {
 
-
     private val REQUEST_CODE_READ_PHONE_STATE = 100
+    private lateinit var database: DatabaseReference
+    private var secretKey: SecretKey? = null
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private fun checkAndRequestPermissions() {
@@ -54,6 +64,14 @@ class MainActivity : AppCompatActivity() {
 
         checkAndRequestPermissions()
 
+        // Firebase 데이터베이스 초기화
+        //val firebaseUrl = "https://haha-f3b7a-default-rtdb.firebaseio.com/"
+        val firebaseUrl = "https://nfckt-b7c41-default-rtdb.firebaseio.com/" //이희우
+        database = FirebaseDatabase.getInstance(firebaseUrl).reference
+
+        // 암호화 키 로드
+        secretKey = loadKey()
+
         binding.button.setOnClickListener {
             val inputPhoneNumber = binding.editTextPhone.text.toString()
             if (inputPhoneNumber.isNotBlank()) {
@@ -63,15 +81,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
-        binding.ManagerButton.setOnClickListener{
-
+        binding.ManagerButton.setOnClickListener {
             val intent = Intent(this, ManagerActivity::class.java)
             startActivity(intent)
         }
-
-
-
     }
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -88,20 +101,10 @@ class MainActivity : AppCompatActivity() {
                 var phoneNumber = subscriptionManager.getPhoneNumber(SubscriptionManager.DEFAULT_SUBSCRIPTION_ID)
 
                 if(phoneNumber.startsWith("+"))
-                    phoneNumber=phoneNumber.replace("+82","0") //수정
+                    phoneNumber = phoneNumber.replace("+82", "0")
 
-                if (phoneNumber != null) {
-                    if (inputPhoneNumber == phoneNumber) {
-                        Toast.makeText(this, "입력한 번호가 유심에 저장된 번호와 일치합니다.", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this, UserActivity::class.java)
-                        intent.putExtra("PHONE_NUMBER", phoneNumber)
-                        startActivity(intent)
-                    }else if(inputPhoneNumber.equals("0")){
-                        val intent = Intent(this, AdminActivity::class.java)
-                        startActivity(intent)
-                    }else {
-                        Toast.makeText(this, "입력한 번호가 유심에 저장된 번호와 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
-                    }
+                if (phoneNumber != null && inputPhoneNumber==phoneNumber) {
+                    checkPhoneNumberInFirebase(inputPhoneNumber, phoneNumber)
                 } else {
                     Toast.makeText(this, "유심에 저장된 번호를 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
                 }
@@ -113,23 +116,73 @@ class MainActivity : AppCompatActivity() {
             var phoneNumber = telephonyManager.line1Number
 
             if(phoneNumber.startsWith("+"))
-                phoneNumber=phoneNumber.replace("+82","0") //수정
+                phoneNumber = phoneNumber.replace("+82", "0")
 
-            if (phoneNumber != null) {
-                if (inputPhoneNumber == phoneNumber) {
-                    Toast.makeText(this, "입력한 번호가 유심에 저장된 번호와 일치합니다.", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, UserActivity::class.java)
-                    intent.putExtra("PHONE_NUMBER", phoneNumber)
-                    startActivity(intent)
-                }else if(inputPhoneNumber.equals("0")){
-                    val intent = Intent(this, AdminActivity::class.java)
-                    startActivity(intent)
-                }else {
-                    Toast.makeText(this, "입력한 번호가 유심에 저장된 번호와 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
-                }
+            if (phoneNumber != null && inputPhoneNumber==phoneNumber) {
+                checkPhoneNumberInFirebase(inputPhoneNumber, phoneNumber)
             } else {
-                Toast.makeText(this, "유심에 저장된 번호를 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "유심에 저장된 번호와 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun checkPhoneNumberInFirebase(inputPhoneNumber: String, phoneNumber: String) {
+        var Key: String? = null
+        if (secretKey == null) {
+            Toast.makeText(this, "암호화 키를 로드할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        database.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var isAuthenticated = false
+                for (childSnapshot in snapshot.children) {
+                    val encryptedKey = childSnapshot.child("key").getValue(String::class.java) ?: ""
+                    Key = EncryptionUtil.decrypt(encryptedKey, secretKey!!)
+
+                    val encryptedValue = childSnapshot.child("value").getValue(String::class.java) ?: ""
+                    try {
+                        val value = EncryptionUtil.decrypt(encryptedValue, secretKey!!)
+                        if (inputPhoneNumber == value) {
+                            isAuthenticated = true
+                            break
+                        }
+                    } catch (e: BadPaddingException) {
+                        Log.e("EncryptionError", "Error decrypting data", e)
+                    }
+                }
+
+                if (isAuthenticated) {
+                    Toast.makeText(this@MainActivity, "${Key}님 환영합니다.", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@MainActivity, UserActivity::class.java)
+                    intent.putExtra("PHONE_NUMBER", phoneNumber)
+                    startActivity(intent)
+                } else if (inputPhoneNumber == "0") {
+                    val intent = Intent(this@MainActivity, AdminActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@MainActivity, "입력한 번호가 회원 번호와 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("FirebaseError", "Error fetching data", error.toException())
+            }
+        })
+    }
+
+    private fun saveKey(key: SecretKey) {
+        val sharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val encodedKey = Base64.encodeToString(key.encoded, Base64.DEFAULT)
+        editor.putString("encryption_key", encodedKey)
+        editor.apply()
+    }
+
+    private fun loadKey(): SecretKey? {
+        val sharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        val encodedKey = sharedPreferences.getString("encryption_key", null) ?: return null
+        val decodedKey = Base64.decode(encodedKey, Base64.DEFAULT)
+        return SecretKeySpec(decodedKey, "AES")
     }
 }
